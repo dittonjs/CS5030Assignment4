7d84d13ac9f9e35e58f7fc8f5d5a709d5a753557
Joseph Ditton <u6047201@notchpeak2.int.chpc.utah.edu>
2022-10-27 10:41:51 -0600

    adds comments

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Assignment 4
+## Setup
+On notchpeak make sure to run `module load cuda`
+
+## Compilation
+`nvcc main.cu -o gc_conversion`
--- main.cu
+++ main.cu
@@ -1,10 +1,3 @@
-/*
- * Hello world cuda
- *
- * compile: nvcc main.cu -o hello
- *
- *  */
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -32,36 +25,45 @@ void RGBToGrayscale(unsigned char * grayImage, unsigned char * rgbImage, int wid
 }
 
 int main() {
-    // Launch GPU kernel
+
+    // READ IMAGE
     int image_size = 1024 * 1024;
     unsigned char * image_h = (unsigned char *)malloc((image_size * 3));
-    unsigned char * gray_image_h = (unsigned char *)calloc(image_size, 1);
     FILE *file = fopen("gc_conv_1024x1024.raw", "rb");
     fread(image_h, (image_size * 3), 1, file);
     fclose(file);
 
+    // ALLOCATE MEMORY
+    unsigned char * gray_image_h = (unsigned char *)calloc(image_size, 1);
     unsigned char * image_d, * gray_image_d;
     cudaMalloc((void **) &image_d, image_size * 3);
     cudaMalloc((void **) &gray_image_d, image_size);
     cudaDeviceSynchronize();
 
+    // COPY IMAGE TO DEVICE
     cudaMemcpy( image_d, image_h, image_size * 3, cudaMemcpyHostToDevice );
     cudaDeviceSynchronize();
 
+    // INITIALIZE BLOCK DIMENTIONS
     dim3 DimGrid(ceil(1024/8), ceil(1024/8), 1);
     dim3 DimBlock(8, 8, 1);
 
+    // RUN DEVICE CODE
     RGBToGrayscale<<< DimGrid, DimBlock >>>(gray_image_d, image_d, 1024, 1024);
 
     cudaDeviceSynchronize();
 
+    // COPY GRAYSCALE BACK INTO HOST MEMORY
     cudaMemcpy(gray_image_h, gray_image_d, image_size, cudaMemcpyDeviceToHost);
 
     cudaDeviceSynchronize();
 
+    // WRITE THE FILE!
     FILE *outfile = fopen("gc.raw", "wb");
     fwrite(gray_image_h, 1, image_size, outfile);
     fclose(outfile);
+
+    // UN ALLOCATE MEMORY
     free(image_h);
     free(gray_image_h);
     cudaFree(image_d);
